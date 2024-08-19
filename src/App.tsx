7c19b2b8b4052c475b0367f3bea6bd2a89d7168f
Sibindra <timalsinasibindra@gmail.com>
2024-08-19 18:55:44 +0545

    fix: test fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from '@app/components/atoms/Button';
 function App() {
   return (
     <div className="border w-screen h-screen flex justify-center items-center">
-      <Button children={'Click Me'} variant={'primary'} />
+      <Button size='md' children={'Click Me'} variant={'primary'} />
     </div>
   );
 }
